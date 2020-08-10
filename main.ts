a70c01071848285b9fb93f21dd0a834c93f417d9
Vineet <vineetsai@gmail.com>
2020-08-09 19:37:54 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+	basic.showNumber(Math.abs(input.rotation(Rotation.Pitch)))
+})
+input.rotation(Rotation.Pitch)
+// console.log("" + (input.rotation(Rotation.Pitch)))
+basic.forever(function () {
+	
+})
